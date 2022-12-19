2993cdd51c57ce59b793346b415947dbf74c8d6c
Livio Alvarenga <livioalvarenga@gmail.com>
2022-12-19 16:53:55 -0300

    aumentado espaçamento clock

--- src/components/Timer/Clock/index.tsx
+++ src/components/Timer/Clock/index.tsx
@@ -1,6 +1,6 @@
 export default function Clock() {
   return (
-    <div className="bg-gradient-to-r from-yellow-300 to-cyan-400 bg-clip-text text-center text-7xl font-bold text-transparent">
+    <div className="bg-gradient-to-r from-yellow-300 to-cyan-400 bg-clip-text text-center text-7xl font-bold tracking-widest text-transparent">
       <span>0</span>
       <span>0</span>
       <span>:</span>
--- src/components/Timer/index.tsx
+++ src/components/Timer/index.tsx
@@ -4,9 +4,7 @@ import Clock from "./Clock";
 export default function Timer() {
   return (
     <div className="flex flex-col items-center justify-center gap-5">
-      <p className="text-center text-4xl md:text-5xl">
-        Escolha um card e inicie o cronometro
-      </p>
+      <p className="text-center text-4xl">Escolha um card e inicie o cronometro</p>
       <Clock />
       <Button hover="hover:bg-red-600">Começar!</Button>
     </div>

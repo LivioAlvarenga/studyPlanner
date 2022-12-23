008eedf6222ee001c410a459bac873c98e892455
Livio Alvarenga <livioalvarenga@gmail.com>
2022-12-23 15:22:42 -0300

    enviado common para src

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,24 +1,21 @@
 import React from "react";
 
-class Button extends React.Component<{
-  children: string;
-  hover?: string | undefined;
+interface Props {
   type?: "button" | "submit" | "reset" | undefined;
   onClick?: () => void;
-}> {
-  render() {
-    const hoverColor = this.props.hover ?? "hover:bg-neutral-900";
-    const type = this.props.type ?? "button";
-    return (
-      <button
-        className={` self-center justify-self-center rounded-xl bg-neutral-800 px-6 py-3 text-xl tracking-wider text-white shadow-md duration-500  ease-in-out  hover:px-9  hover:shadow-neutral-800 ${hoverColor}`}
-        type={type}
-        onClick={this.props.onClick}
-      >
-        {this.props.children}
-      </button>
-    );
-  }
+  hover?: string | undefined;
+  children?: React.ReactNode;
 }
 
-export default Button;
+export default function Button({ type, onClick, hover, children }: Props) {
+  const hoverColor = hover ?? "hover:bg-neutral-900";
+  return (
+    <button
+      className={` self-center justify-self-center rounded-xl bg-neutral-800 px-6 py-3 text-xl tracking-wider text-white shadow-md duration-500  ease-in-out  hover:px-9  hover:shadow-neutral-800 ${hoverColor}`}
+      type={type ?? "button"}
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  );
+}

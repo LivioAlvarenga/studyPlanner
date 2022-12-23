74040b8017fc8d7c66e4a848ca7847e7388d0f4a
Livio Alvarenga <livioalvarenga@gmail.com>
2022-12-23 10:43:04 -0300

    criando function timeToSecond

--- src/components/Timer/index.tsx
+++ src/components/Timer/index.tsx
@@ -1,7 +1,9 @@
 import Button from "../Button";
+import { timeToSecond } from "../common/utils/date";
 import Clock from "./Clock";
 
 export default function Timer() {
+  console.log("time to second: ", timeToSecond("01:01:01"));
   return (
     <div className="flex flex-col items-center justify-center gap-5">
       <p className="text-center text-4xl">Escolha um card e inicie o cronometro</p>
